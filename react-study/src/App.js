89e73d4d079797673fe2cb64c23b66c4a838ccb4
Jiun <jiun48562@gmail.com>
2022-02-03 03:39:04 +0900

    7강 코드챌린지

--- react-study/src/App.js
+++ react-study/src/App.js
@@ -1,9 +1,53 @@
-
+import {useState, useEffect} from "react";
 
 function App() {
+  const[loading, setLoading] = useState(true);
+  const[coins, setCoins] = useState([]);
+  const[value, setValue] = useState(0);
+  const[selected, setSelected] = useState("btc-bitcoin");
+  const[endConvert, setConverted] = useState(false);
+  let converted = 0;
+  let showConverted;
+
+  const onChangeInput = (event) => {
+    setValue(event.target.value);
+  }
+
+  const onClick = () => {
+    const selectedCoin = coins.find((coin) => coin.id === selected);
+    converted = value / selectedCoin.quotes.USD.price;
+    showConverted = `${converted}` + selectedCoin.symbol;
+    console.log(showConverted);
+    setConverted(true);
+  }
+
+  const onChangeSelect = (event) => {
+    setSelected(event.target.value);
+  }
+
+
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers").then((response) => response.json())
+    .then((json) => {
+      setCoins(json);
+      setLoading(false);
+    })
+  }, [])
+
   return (
     <div>
-      <h1>Welcome back!</h1>
+      <h1>Coin Converter</h1>
+      {loading ? 
+        <div className="spinner-border m-5" role="status"><span className="visually-hidden">Loading...</span></div> : 
+        <select onChange={onChangeSelect}>{coins.map((coin) => <option value={coin.id}>{coin.name} ({coin.symbol})</option>)}</select>
+      }
+      <br />
+      <div>
+        <input type="number" onChange={onChangeInput} placeholder="type USD"></input>
+        <button onClick={onClick}>Convert</button>
+      </div>
+      <br />
+      {endConvert ? <span>You can buy {showConverted}</span> : null}
     </div>
   );
 }
